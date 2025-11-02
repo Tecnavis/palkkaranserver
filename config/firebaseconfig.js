01cca25da8a0ba5b4f1d191ad462488fe91485d3
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-11-02 14:39:03 +0530

    fix: firebase issue

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -1,13 +1,27 @@
 
-const admin = require('firebase-admin');
-// const serviceAccount = require('../fibaseservice.json');
+// const admin = require('firebase-admin');
+// // const serviceAccount = require('../fibaseservice.json');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+// admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+// });
+
+// const messaging = admin.messaging();
+
+// module.exports = messaging;
+
+
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_CONFIG_BASE64, 'base64').toString('utf8')
+);
 
 admin.initializeApp({
- credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const messaging = admin.messaging();
-
 module.exports = messaging;
