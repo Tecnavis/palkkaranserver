99911e3251f84fa656e42d8078072e0f1b96c97d
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-11-03 07:49:52 +0530

    fix: new

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "js")));
+
+
 
 
 
